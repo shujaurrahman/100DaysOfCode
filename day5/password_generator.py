72ae07dd14bcce5b5f53886a6d79fa2a0cbbe56f
shujaurrahman <Shujaurrehman210@gmail.com>
2024-03-18 00:07:13 +0530

    Day 5 Final project

---
+++ day5/password_generator.py
@@ -0,0 +1,37 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+l=int(input("How many letters would you like in your password? \n "))
+num=int(input("How many numbers would you like in your password? \n "))
+sym=int(input("How many symbols would you like in your password? \n "))
+
+# EasyLevel
+password=""
+for char in range(1,l+1):
+    password+=random.choice(letters)
+for char in range(1,num+1):
+    password+=random.choice(numbers)
+for char in range(1,sym+1):
+    password+=random.choice(symbols)
+
+print(f"Your Password is : {str(password)}")
+
+#hard level
+password_list=[]
+for char in range(1,l+1):
+    password_list+=random.choice(letters)
+for char in range(1,num+1):
+    password_list+=random.choice(numbers)
+for char in range(1,sym+1):
+    password_list+=random.choice(symbols)
+random.shuffle(password_list)
+# print(password_list)
+
+# convert list into string 
+passwordshuffled=""
+for char in password_list:
+    passwordshuffled+=char
+print("Your Shuffled Password is : "+passwordshuffled)
