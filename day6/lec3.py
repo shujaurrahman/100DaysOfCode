e4525d909d370a3dec3f432b9482ba206188a6cf
shujaurrahman <Shujaurrehman210@gmail.com>
2024-03-18 23:53:43 +0530

    Lecture 3

---
+++ day6/lec3.py
@@ -0,0 +1,10 @@
+# While loop
+# while(something is true)
+    # do somethimng
+
+# like previous prob
+num_of_hurdle=6
+while num_of_hurdle>0:
+    # jump()
+    num_of_hurdle-=1
+    print("Jumped!")
