d03246174a0b190a3689b1e5bf6dd72119b1cd83
shujaurrahman <Shujaurrehman210@gmail.com>
2024-03-21 05:18:53 +0530

    Lec 2

---
+++ day8/lec2.py
@@ -0,0 +1,8 @@
+def greet_with(name,loc):
+    print(f"Hello, {name}")
+    print(f"How is weather in {loc}")
+
+greet_with("shuja","pilibhit")
+
+# postional arguments parameter with arguments given to functionn in any order
+greet_with(loc="delhi",name="shuja")
