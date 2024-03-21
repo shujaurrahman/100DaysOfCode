fd7e36e4143ed15c62ff4f68d9c236a90448c8a3
shujaurrahman <Shujaurrehman210@gmail.com>
2024-03-22 03:59:40 +0530

    lec 3

---
+++ day9/lec3.py
@@ -0,0 +1,10 @@
+# Nesting in list and dictionary 
+# we can next dictionar and list withn key value of the dictionary example
+dictionary={
+    "Name":["shuja","yusuf","imran"],
+    "deparments and courses":{
+        "shuja's department":"computer Science",
+        "yusuf's department":"Computer Science",
+        "Imran's department":"Chemistry "
+        }
+}
