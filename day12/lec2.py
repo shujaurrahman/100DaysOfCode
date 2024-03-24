b5fbbe0cad5480acba1c456dd80c9c57d8b64311
shujaurrahman <Shujaurrehman210@gmail.com>
2024-03-25 01:35:23 +0530

    lec 2

---
+++ day12/lec2.py
@@ -0,0 +1,10 @@
+# there is no block scope in python 
+#like in cpp or java there is block scope 
+#example 
+# {
+#     a=5
+#     cout<<"a";
+
+# }
+
+# in cpp it is not accessible outof this scope a variable
