33be7e86f43bf2808ad5adab4e9a39b4bdb329fb
shujaurrahman <Shujaurrehman210@gmail.com>
2024-03-25 01:35:10 +0530

    lec 1

---
+++ day12/lec1.py
@@ -0,0 +1,9 @@
+enimies=1 #global variable
+def enimy():
+    enimies=50 #local variable
+    print(f"No of enimies are : {enimies}")
+
+enimy()
+print(enimies)
+
+# this will print 1 as enimies inside function is out of scope
