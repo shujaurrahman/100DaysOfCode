8a2a6b2a351aabe270f1fb18692ee736fc6f190a
shujaurrahman <Shujaurrehman210@gmail.com>
2024-03-31 20:25:22 +0530

    lec 1

---
+++ day18/lec1.py
@@ -0,0 +1,12 @@
+from turtle import Turtle,Screen
+
+timmy_the_turle=Turtle()
+screen=Screen()
+
+timmy_the_turle.shape("turtle")
+timmy_the_turle.forward(100)
+timmy_the_turle.color("red")
+timmy_the_turle.right(90)
+timmy_the_turle.forward(100)
+timmy_the_turle.reset()
+screen.exitonclick()
