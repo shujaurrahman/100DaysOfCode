4d2416ed0237a5a14d41ba907469bc83790bb7de
shujaurrahman <Shujaurrehman210@gmail.com>
2024-04-21 12:50:01 +0530

    day23

--- day23/turtle-crossing-start/main.py
+++ day23/turtle-crossing-start/main.py
@@ -9,9 +9,35 @@ screen.setup(width=600, height=600)
 screen.title("Turtle crossing game")
 screen.tracer(0)
 
+player=Player()
+car_manager=CarManager()
+scoreboard=Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up,"Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
 
-print("Alkaram")
+    #create car in update 
+    car_manager.create_car()
+    car_manager.move_car()
+
+    #Detect collision with car
+
+    for car in car_manager.all_cars:
+        if car.distance(player)<20:
+            game_is_on=False
+            scoreboard.game_over()
+
+    #Detect successfull crossing of player 
+    if player.is_at_finish_line():
+        player.goto_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+    
+
+screen.exitonclick()
+
