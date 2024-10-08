413f8ef8451ea052b4129d9c6a8a4a9944cce81f
shujaurrahman <Shujaurrehman210@gmail.com>
2024-10-09 03:19:46 +0530

    day25

--- day25/day-25-us-states-game-start/main.py
+++ day25/day-25-us-states-game-start/main.py
@@ -16,10 +16,11 @@ while len(gussed_state)<50:
     answer_state=screen.textinput(title=f"{len(gussed_state)}/50 states correct ",prompt="what's another state's name?").title()
 
     if answer_state=="Exit":
-        missing_state=[]
-        for state in all_states:
-            if state not in gussed_state:
-                missing_state.append(state)
+        # shortening using list comprehension
+        missing_state=[state for state in all_states if state not in gussed_state]  
+        # for state in all_states:
+        #     if state not in gussed_state:
+        #         missing_state.append(state)
         new_data=pandas.DataFrame(missing_state)
         new_data.to_csv("day25/day-25-us-states-game-start/states_to_learn.csv")
         break
