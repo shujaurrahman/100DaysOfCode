189a4798478ef971494b068b69d51dd405fb309e
shujaurrahman <Shujaurrehman210@gmail.com>
2024-10-12 20:21:10 +0530

    day26

--- day26/main.py
+++ day26/main.py
@@ -18,13 +18,38 @@
 # new_dict={newkey : new_value for item in list }
 # new_dict={new_key:new_value for (key,value) in dict.items() if test }
 
-names=["shuja","Mariyam","Aqsa"]
-import random
-student_scores={student:random.randint(1,100) for student in names}
-print(student_scores)
+# names=["shuja","Mariyam","Aqsa"]
+# import random
+# student_scores={student:random.randint(1,100) for student in names}
+# print(student_scores)
 
 
-passed_students={student:score for (student,score) in student_scores.items() if score>=40}
+# passed_students={student:score for (student,score) in student_scores.items() if score>=40}
 
-print(passed_students)
+# print(passed_students)
 
+
+student_dict={
+    "student":["shuja","yusuf","saud"],
+    "score":[56,76,80]
+}
+
+# for (key,value) in student_dict.items():
+#     print(key)
+
+import pandas
+
+
+student_data_frame=pandas.DataFrame(student_dict)
+print(student_data_frame)
+# Loop through a dataframe 
+# for (key,value) in student_data_frame.items():
+#     print(value)
+
+
+for index,row in student_data_frame.iterrows():
+    # print(index)
+    # print(row)
+    # print(row.score)
+    if row.student=="shuja":
+        print(row.score)
